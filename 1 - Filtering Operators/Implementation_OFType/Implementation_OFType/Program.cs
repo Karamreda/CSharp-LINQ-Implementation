ce6d4b136f1b255aabf4ac6690cc2b6e0931cb28
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-02 08:56:29 +0300

    Add files via upload

---
+++ 1 - Filtering Operators/Implementation_OFType/Implementation_OFType/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+
+namespace Implementation_OFType
+{
+    #region OfType() inLINQ
+
+    /*
+     What is OfType() in LINQ?
+     >>>>> Filter items by data type 
+     بيرجع العناصر من نوع معين فقط
+
+    Example: We have a list of objects that contains different types of data,
+    such as integers, strings, and doubles. 
+    We want to filter out only the String from this list
+
+    // C# code 
+    ////////////////////////////////////////////////////////
+    ArrayList list =[ 1, "Karam" , 2 , "Said" , 3.5 , true];
+    list.OfType<string>()
+    ////////////////////////////////////////////////////////
+    
+    // the output will be: Karam , Said
+    
+    // using OfType() in LINQ, we can easily filter out the elements of a specific type from a collection.
+      
+      
+      
+     
+     */
+
+
+
+
+
+    #endregion
+    public static class MyLINQ_OfType_Extension{
+        public static IEnumerable<TResult> MyOfType<TResult>(this IEnumerable source)
+        {
+            foreach (object item in source) 
+            {
+                if (item is TResult) // Check if the item is of the specified type
+                {
+                    yield return (TResult)item; // Cast the item to the specified type and yield it
+                }
+                // anther if you can use it like this
+
+                //if (item is TResult result)
+                //{
+                //    yield return result;
+                //}
+               
+
+
+            }
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList { 1, "Karam", 2, "Said", 3.5, true };
+            var filteredList = list.MyOfType<string>(); // TResult = String
+            foreach (string item in filteredList)
+            {
+                Console.WriteLine("The filtered item is: " + item);
+            }
+        }
+    }
+}
---
+++ 1 - Filtering Operators/Implementation_Where/Implementation_Where/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Implementation_Where
+{
+
+
+    #region  Where() in LINQ 
+    /*
+     * 
+     What is the Where() method in C#? 
+     >>>> Give me all the elements that meet a specific condition.
+     >>>>> هاتلي كل العناصر اللي تحقق شرط معين
+
+     // Example: 
+     We have a list of numbers and we want to get all the even numbers from that list. 
+     We can use the Where method to filter the list based on the condition that the number is even.
+
+    // C# code:
+    ////////////////////////////////////////////////////////
+     var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+     var result = numbers.Where(x => x % 2 == 0);
+    ////////////////////////////////////////////////////////
+
+    // the output will be: 2, 4, 6
+
+    // Using the Linq Where() method to filter a list of numbers based on a condition
+
+     */
+    #endregion
+
+    public static class MyLINQ_Where_Extensions
+    {
+
+        // create a method called MyWhere
+        // takes an >>>>>> IEnumerable<T> and a >>>>>>>>>>  predicate function as parameters (Condition) 
+        public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> Condition )
+        {
+            foreach (T item in source) 
+            {
+                if (Condition(item)) //check if the item meets the condition
+                {
+                    yield return item; //return the item if it meets the condition
+                }
+              
+            }
+        }
+
+    }
+   
+    public class Student // Class for example2
+    {
+
+    
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    
+    }
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            // Example 1 
+
+            List<int> numbers =[1,2,3,4,5,6,7,8]; // source
+
+            var even = numbers.MyWhere(x => x % 2 == 0); // Filtering condition: Get all even numbers
+
+            foreach (var item in even)
+            {
+                Console.WriteLine("The even number is: " + item);
+            }
+
+
+
+
+            // Example 2: Using MyWhere() with a list of students
+
+            List<Student> students =[
+                new Student{Name="Karam",Grade=50},
+                new Student{Name="Yasmin",Grade=90},
+                new Student{Name="Hussin",Grade=95}
+                ];
+
+            var passed = students.MyWhere(s => s.Grade >= 60);
+
+            foreach (var student in passed)
+            {
+                Console.WriteLine("The student who passed is: " + student.Name);
+            }
+         
+        }
+    }
+}
