d6a8fea5b6f2200ac9416fb09771c9210c6a9797
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-03 03:37:04 +0300

    Add files via upload

---
+++ 4 - Aggregation Operators/Implementation_Aggregation/Implementation_Aggregation/MyExtensionMethods.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implementation_Aggregation
+{
+
+
+
+    public static class MyExtensionMethods
+    {
+        // Count() >>>>>> Count the number of elements in a collection
+        public static int MyCount<T>(this IEnumerable<T> source)
+        {
+            int count = 0;
+
+            foreach (var item in source)
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+
+
+        // Sum() >>>>>> Calculate the sum of a collection of numbers
+        public static int MySum(this IEnumerable<int> source)
+        {
+            int sum = 0;
+
+            foreach (var item in source)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+
+
+        // Average() >>>>>>> Calculate the average of a collection of numbers
+        public static double MyAverage(this IEnumerable<int> source)
+        {
+            int sum = 0;
+            int count = 0;
+
+
+            foreach (var item in source)
+            {
+                sum += item;
+                count++;
+            }
+
+
+            return (double)sum / count;
+        }
+
+
+
+        // Min() >>>>>> Find the minimum value in a collection of numbers
+        public static int MyMin(this IEnumerable<int> source)
+        {
+            int min = int.MaxValue;
+
+
+            foreach (var item in source)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+
+
+            return min;
+        }
+
+
+
+        // Max() >>>> Find the maximum value in a collection of numbers
+        public static int MyMax(this IEnumerable<int> source)
+        {
+            int max = int.MinValue;
+
+
+            foreach (var item in source)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+
+
+            return max;
+        }
+
+
+
+        // Aggregate() >>>>>> Apply an accumulator function over a collection
+        public static int MyAggregate(this IEnumerable<int> source,Func<int, int, int> func)
+        {
+
+            int result = 1;
+
+
+            foreach (var item in source)
+            {
+                result = func(result, item);
+            }
+
+            return result;
+        }
+
+    }
+}
+
+
+
+
---
+++ 4 - Aggregation Operators/Implementation_Aggregation/Implementation_Aggregation/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Implementation_Aggregation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<int> numbers = new(){ 10, 20, 30, 40, 50 }; // Sample collection of numbers
+
+
+
+
+            // Count
+            var count = numbers.MyCount();
+
+            Console.WriteLine($"Count = {count}"); // output: Count = 5
+
+
+
+            // Sum
+            var sum = numbers.MySum();
+
+            Console.WriteLine($"Sum = {sum}"); // output: Sum = 150
+
+
+
+            // Average
+            var avg = numbers.MyAverage();
+
+            Console.WriteLine($"Average = {avg}"); // output: Average = 30
+
+
+
+            // Min
+            var min = numbers.MyMin();
+
+            Console.WriteLine($"Min = {min}"); // output: Min = 10
+
+
+
+            // Max
+            var max = numbers.MyMax();
+
+            Console.WriteLine($"Max = {max}"); // output: Max = 50
+
+
+
+            // Aggregate
+            var multiply = numbers.MyAggregate(  (total, number) => total * number);
+
+            Console.WriteLine($"Aggregate Multiply = {multiply}"); // output: Aggregate Multiply = 12000000
+        }
+    }
+}
