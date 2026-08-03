ed512a9a0c26a89af6625b72073e82a4f165cd8f
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-03 07:59:25 +0300

    Add files via upload

---
+++ 2 - Projection Operators/Implementation_Select/Implementation_Select/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Implementation_Select
+{
+    #region Select() in LINQ
+
+    /*
+     What is Select() in LINQ?
+     >>>>>>> It transforms every item in the Collection into a new form
+    بيحول كل عنصر في المجموعة إلى شكل جديد
+
+    // Example : We have a list of numbers and we want to get the square of each number using Select() in LINQ.
+
+    // C# code with Select() in LINQ
+    ////////////////////////////////////////////////////////////////////////////////
+
+    List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+    var squares = numbers.Select(x => x * x);
+    foreach (var square in squares)
+    {
+        Console.WriteLine(square);
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    // Output: 1 4 9 16 25
+    
+     */
+
+
+
+
+    #endregion
+
+    public static class MyLINQ_Select_Extensions
+    {
+        public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            foreach (var item in source)
+            {
+                yield return selector(item); // Transform each item using the provided selector function
+            }
+        }
+
+        // TSource: The type of the elements in the source collection.
+        // TResult: The type of the elements in the resulting collection.
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1: Using MySelect() to get the squares of numbers in a list
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 }; // Source collection 
+            var squares = numbers.MySelect(x => x * x); // the TSource is int and TResult is int 
+            foreach (var square in squares)
+            {
+                Console.WriteLine("The square of is : " + square);
+            }
+
+            ////////////////////////////////////////////////////////
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine();
+            ////////////////////////////////////////////////////////
+
+            // Example 2: Using MySelect() to transform a list of strings to their lengths
+
+            List<string> words = new List<string> { "Karam", "Yasmin", "Said" };
+            var lengths = words.MySelect(word => word.Length); // the TSource is string and TResult is int
+            foreach (var item in lengths)
+            {
+                Console.WriteLine("The length of is : " + item);
+            }
+
+
+        }
+    }
+}
---
+++ 2 - Projection Operators/Implementation_SelectMany/Implementation_SelectMany/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Implementation_SelectMany
+{
+
+    #region SelectMany() in LINQ
+    /*
+     What is SelectMany() in LINQ?
+    >>>>>>>> If you have a Collection that contains Collections, 
+             it will bring them all together into one Collection
+
+     // Example: We have a collection of Numbers, and each index has a collection of Lists Number.
+  
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    List<int> numbers = new List<int> { new List<int> {1,2}, new List<int> {3,4}, new List<int> {5,6} };
+    var result = numbers.SelectMany(x => x)
+    foreach (var item in result)
+    {
+        Console.WriteLine(item);
+    }
+    
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // output: 1,2,3,4,5,6
+
+
+     */
+
+
+
+    #endregion
+
+    public static class MyLINQ_SelectMany_Extensions
+    {
+        public static IEnumerable<TResult> MySelectMany<TSource, TResult>
+        (
+        this IEnumerable<TSource> source, // The collection of collections
+        Func<TSource, IEnumerable<TResult>> selector // x => x
+        )
+        {
+            foreach (var item in source) // Any collection of TSource
+            {
+                foreach (var value in selector(item)) // Any collection of TResult 
+                {
+                    yield return value;
+                }
+            }
+            
+        }
+
+        // TSource : This is the type of original element.
+        // TResult : This is the type of the element in the collection that is returned by the selector function.
+
+    }
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TSource is >>> List<int>
+            // TResult is item >>> int
+
+            List<List<int>> numbers = new List<List<int>> { new List<int> { 1, 2 }, new List<int> { 3, 4 }, new List<int> { 5, 6 } }; 
+            var result = numbers.MySelectMany(x => x);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
