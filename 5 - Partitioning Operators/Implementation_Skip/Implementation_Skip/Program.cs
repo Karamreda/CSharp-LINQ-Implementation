857d5826934931b4a8425713accc14ad4998cbff
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-10 23:54:01 +0300

    Add files via upload

---
+++ 5 - Partitioning Operators/Implementation_Skip/Implementation_Skip/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Implementation_Skip
+{
+    #region Skip() in LINQ
+    /*
+    What is the Skip() in LINQ ?
+    It ignores the first n elements and returns the rest.
+    بتتجاهل اول عدد معين من العناصر و ترجع الباقي
+
+
+
+    //Example : We have a Collection of number we want skip the first 3 element 
+    //C# code with Linq
+
+    /////////////////////////////////////////////////////////////////////////////////////
+    
+    
+    int[] numbers = { 2, 4, 6, 7, 8, 10 };
+    var result = numbers.Skip(2);
+    foreach (var number in result)
+    {
+       Console.WriteLine(number);
+    }
+ 
+    /////////////////////////////////////////////////////////////////////////////////////
+
+
+    */
+    #endregion
+
+    public static class MyLinq_Skip_Extetion
+    {
+
+        public static IEnumerable<int> MySkip(this IEnumerable<int> source, int count) 
+        {
+            int skipped = 0;
+
+            foreach (int item in source) 
+            {
+                if (skipped < count) 
+                {
+                    skipped++;
+                    continue;
+                }
+
+                yield return item;
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 4, 6, 7, 8, 10 };
+            var result = numbers.MySkip(2);
+            foreach (var number in result)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ 5 - Partitioning Operators/Implementation_Take/Implementation_Take/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Implementation_Take
+{
+    #region Take() in LINQ
+    /*
+     What is Take() in LINQ?
+    It takes the first n elements from the Collection.
+    بتاخد أول عنصر عناصر من المجموعة
+
+
+    // Example : We have a list of numbers and we want to take the first 3 numbers from the list.
+    // C# Code:
+    //////////////////////////////////////////////////////////////////////
+    
+    int[] numbers = { 10, 20, 30, 40, 50 };
+    var result = numbers.Take(3);
+    foreach (var number in result)
+    {
+       Console.WriteLine(number);
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // Output: 10 20 30
+
+
+
+    */
+    #endregion
+
+
+    public static class MyLINQ_Take_Extensions
+    {
+        public static IEnumerable<int> MyTake(this IEnumerable<int> source, int count) // take first n elements from the collection >>> count = n
+        {
+            
+            int taken = 0; 
+
+            foreach (int item in source) 
+            {
+                if (taken >= count)
+                    yield break;
+
+                yield return item;
+                taken++;
+            }
+        }
+    }
+
+
+
+
+
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            var result = numbers.MyTake(3);
+
+            foreach (var number in result)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
