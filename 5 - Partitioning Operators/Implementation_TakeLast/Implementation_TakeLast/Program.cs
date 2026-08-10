6aaec315730f97bbc462c8a174f03126145000e3
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-10 23:56:09 +0300

    Add files via upload

---
+++ 5 - Partitioning Operators/Implementation_TakeLast/Implementation_TakeLast/Program.cs
@@ -0,0 +1,88 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Implementation_TakeLast
+{
+    #region Takelast() in LINQ
+    /*
+    What is TakeLast() in LINQ?
+    It takes the last n elements
+    بتاخد اخر عدد معين من العناصر 
+     
+    // Example : We have a list of numbers and we want to take the last 3 elements from that list. We can use the TakeLast() method to achieve this.
+    // C# code Without LINQ
+
+    ////////////////////////////////////////////////////////////////////////////
+    int[] numbers = { 10, 20, 30, 40, 50 };
+    var result = numbers.TakeLast(3);
+    foreach (var number in result)
+    {
+        Console.WriteLine(number);
+    }
+
+
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // output: 30, 40, 50
+
+
+
+
+
+
+
+    */
+    #endregion
+
+    public static class MyLinq_TakeLast_Extensions
+    {
+
+        public static IEnumerable<int> MyTakeLast(this IEnumerable<int> source, int count)
+        {
+            // count length of the source collection
+            int length = 0;
+
+            foreach (int item in source)
+            {
+                length++;
+            }
+
+            // calculate how many items to skip
+            int skip = length - count;
+
+            if (skip < 0) 
+            // if count is greater than the length of the source collection, return all item >>> dont skip any item
+                skip = 0;
+
+            int index = 0; 
+            
+            foreach (int item in source) 
+            {
+                if (index >= skip) // if the index is greater than or equal to the skip value, yield return the item
+                    yield return item;
+
+                index++;
+            }
+        }
+
+       
+
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            var result = numbers.MyTakeLast(3);
+            foreach (var number in result)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
---
+++ 5 - Partitioning Operators/Implementation_TakeWhile/Implementation_TakeWhile/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Implementation_TakeWhile
+{
+    #region TakeWhile() in LINQ
+    /*
+    What is the TakeWhile() in Linq ?
+    It prefers to take Elements as long as the Condition is met
+    بتاخد العنصر اللي بيحقق شرط
+
+    // Exampel : We have a list of numbers , we want the even number
+    // C# code with LINQ
+
+    ////////////////////////////////////////////////////////////////////
+    
+    int[] numbers = { 1, 2, 3, 4, 5, 6 , 7 ,8 };
+    var result = numbers.TakeWhile(x =>  x % 2 == 0);
+    foreach (var number in result)
+    {
+        Console.WriteLine(number);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+     Output : 2 4 6 8 
+
+
+    */
+    #endregion
+
+    public static class MyMyLinq_TakeWhile_Extensions
+    {
+        public static IEnumerable<int> MyTakeWhile(this IEnumerable<int> source , Func<int, bool> Condition)
+        {
+            foreach (int item in source)
+            {
+                if (!Condition(item)) 
+                    yield break;
+
+                yield return item;
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 2, 4, 6, 7, 8, 10 };
+            var result = numbers.MyTakeWhile(x => x % 2 == 0);
+            foreach (var number in result)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
