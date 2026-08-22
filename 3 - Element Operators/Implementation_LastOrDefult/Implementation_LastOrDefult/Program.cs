6dfeedfec99fbada2bb9ff9989851b9fb9300b68
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-23 01:56:00 +0300

    Add files via upload

---
+++ 3 - Element Operators/Implementation_LastOrDefult/Implementation_LastOrDefult/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Implementation_LastOrDefult
+{
+    #region LastOrDefult() in Linq
+    /*
+    What is the Last() in Linq ?
+    get the last item in the collection >>>>>>>>>>>>> if the collection is empty return defult
+    بتجيب اخر عنصر
+
+    Example : We have collection of number we want the last number
+    // C# code with linq 
+--------------------------------------------------------------------------------------
+
+    int[] numbers = { 10, 20, 30, 40, 50 };
+    int result = numbers.LastOrDefult();
+    Console.WriteLine(result);
+--------------------------------------------------------------------------------------
+    output >>>> 50
+
+    */
+    #endregion
+    public static class Mylinq_LastOrDefult_Extentions
+    {
+        public static int MyLastOrDefult(this IEnumerable<int> source)
+        {
+    
+            int last = default; 
+
+            foreach (int item in source)
+            {
+                last = item;
+            }
+
+            return last;
+        
+    }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            int result = numbers.MyLastOrDefult();
+            Console.WriteLine(result);
+
+            //int[] numbers = {};
+            //int result = numbers.MyLastOrDefult();
+            //Console.WriteLine(result);
+        }
+    }
+}
+
---
+++ 3 - Element Operators/Implementation_Single/Implementation_Single/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Implementation_Single
+{
+    #region Single() in LINQ
+    /*
+    What is the Single() in Linq ?
+    Get one item >> and it must be the only different item in the collection.
+
+    Example : We have a collection of number , we want the different item
+    // C#  code with linq
+
+    ---------------------------------------------------------------------------
+    
+    
+    int[] numbers = { 10 };
+    int result = numbers.Single();
+    Console.WriteLine(result);
+    ----------------------------------------------------------------------------
+    output >>> 10
+
+    If it contains more than one element >>>>>>>>> Expetion
+
+
+    */
+    #endregion
+
+
+    public static class Mylinq_Single_Extentions
+    {
+        public static int MySingle(this IEnumerable<int> source)
+        {
+            int result = default; // return output
+            int count = 0; // count item
+
+            foreach (int item in source)
+            {
+                result = item; // output
+                count++;
+            }
+
+            if (count != 1)  //If it contains more than one element >>>>>>>>> Expetion
+                throw new InvalidOperationException(); // Expetion
+
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10 };
+            int result = numbers.MySingle();
+            Console.WriteLine(result);
+
+
+
+            //int[] numbers = {  10 , 20 , 30 };
+            //int result = numbers.MySingle();
+            //Console.WriteLine(result);
+
+        }
+    }
+}
