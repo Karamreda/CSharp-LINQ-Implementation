b6c6a906694960b71578e106ffab11572c969a34
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-23 01:20:56 +0300

    Add files via upload

---
+++ 3 - Element Operators/Implementation_First/Implementation_First/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Implementation_First
+{
+    #region First() in LINQ
+    /*
+    What is the First in Linq ?
+    get the first item in the Collection >>>>>>>> If the collection is empty, First() will throw an exception 
+    بتجيب اول عنصر 
+
+    Example : We have a array of numbers we want the first item 
+    // C# code with linq 
+
+    -------------------------------------------------------------------------------------
+    
+    int[] numbers = { 10, 20, 30, 40, 50 };
+    int result = numbers.First();
+    Console.WriteLine(result);
+    -------------------------------------------------------------------------------------
+    output >>>>>>>>>>> 10 
+
+    */
+    #endregion
+
+    public static class Mylinq_First_Extensions
+    {
+        public static int MyFirst(this IEnumerable<int> source) // take source collection 
+        {
+            foreach (int item in source)
+            {
+                return item;
+            }
+
+            throw new InvalidOperationException(); // is empty >>>>> throw an exception 
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 10, 20, 30, 40, 50 }; // collection
+            int result = numbers.MyFirst();
+            Console.WriteLine(result);
+
+
+
+
+        }
+    }
+}
---
+++ 3 - Element Operators/Implementation_FirstOrDefult/Implementation_FirstOrDefult/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Implementation_FirstOrDefult
+{
+    #region FirstOrDefult() in LINQ
+    /*
+    What is the FirstOrDefult() in Linq ?
+    get the first item in the Collection >>>>>>>> If the collection is empty, FirstOrDefult() will return defult
+    بتجيب اول عنصر 
+
+    Example : We have a array of numbers we want the first item 
+    // C# code with linq 
+
+    -------------------------------------------------------------------------------------
+    
+    int[] numbers = { 10, 20, 30, 40, 50 };
+    int result = numbers.FirstOrDefult();
+    Console.WriteLine(result);
+    -------------------------------------------------------------------------------------
+    output >>>>>>>>>>> 10 
+
+    */
+    #endregion
+
+    public static class Mylinq_FirstOrDefult_Extensions
+    {
+        public static int MyFirstOrDefult(this IEnumerable<int> source) // take source collection 
+        {
+            foreach (int item in source)
+            {
+                return item;
+            }
+
+            return default;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 10, 20, 30, 40, 50 }; // collection
+            int result = numbers.MyFirstOrDefult();
+            Console.WriteLine(result);
+
+            // if empty
+            //int[] numbers = {}; // collection
+            //int result = numbers.MyFirstOrDefult();
+            //Console.WriteLine(result);
+
+
+
+        }
+    }
+}
