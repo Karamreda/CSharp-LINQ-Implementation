b7f04e1344cc5fd80b232df4095b53e98d67c3da
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-08-23 02:08:40 +0300

    Add files via upload

---
+++ 3 - Element Operators/Implementation_SingleOrDefault/Implementation_SingleOrDefault/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Implementation_SingleOrDefault
+{
+    #region SingleOrDefult() in LINQ
+    /*
+    What is the SingleOrDefult() in Linq ?
+    Get one item >> and it must be the only different item in the collection.
+
+    Example : We have a collection of number , we want the different item
+    // C#  code with linq
+
+    ---------------------------------------------------------------------------
+    
+    
+    int[] numbers = { 10 };
+    int result = numbers.SingleOrDefult();
+    Console.WriteLine(result);
+    ----------------------------------------------------------------------------
+    output >>> 10
+
+    If it contains more than one element >>>>>>>>> Expetion
+
+
+    */
+    #endregion
+
+
+    public static class Mylinq_SingleOrDefult_Extentions
+    {
+        public static int MySingleOrDefult(this IEnumerable<int> source)
+        {
+            int result = default; // return output
+            int count = 0; // count item
+
+            foreach (int item in source)
+            {
+                result = item; // output
+                count++;
+            }
+
+            if (count > 1)  //If it contains more than one element >>>>>>>>> Expetion
+                throw new InvalidOperationException(); // Expetion
+
+            return result;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10 };
+            int result = numbers.MySingleOrDefult();
+            Console.WriteLine(result);
+
+
+
+            //int[] numbers = {  10 , 20 , 30 };
+            //int result = numbers.MySingleOrDefult();
+            //Console.WriteLine(result);
+
+        }
+    }
+}
+
